6042d48753b91dd0465fc091e6e11a805ab161f3
Josue Abraham <josuea@noukod.com>
2019-01-22 19:34:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  #patch 'posts/:id', to: 'posts#update'
+  patch 'posts/:id', to: 'posts#update'
   resources :posts, only: [:index, :show, :new, :create, :edit]
 end
